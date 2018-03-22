8bd2494e557996fd3ce842456bfa99b7f413ed23
Chris MacNaughton <chris@centaurisolutions.nl>
2018-03-22 10:29:32 +0100

    make it actually resolve

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use std::net::UdpSocket;
+use std::io::Result;
+
 mod byte_packet_buffer;
 mod dns_header;
 mod result_code;
@@ -13,3 +16,23 @@ pub use query_type::QueryType;
 pub use dns_record::DnsRecord;
 pub use dns_packet::DnsPacket;
 pub use dns_question::DnsQuestion;
+
+pub fn lookup(qname: &str, qtype: QueryType, server: (&str, u16)) -> Result<DnsPacket> {
+    let socket = try!(UdpSocket::bind(("0.0.0.0", 43210)));
+
+    let mut packet = DnsPacket::new();
+
+    packet.header.id = 6666;
+    packet.header.questions = 1;
+    packet.header.recursion_desired = true;
+    packet.questions.push(DnsQuestion::new(qname.to_string(), qtype));
+
+    let mut req_buffer = BytePacketBuffer::new();
+    packet.write(&mut req_buffer).unwrap();
+    try!(socket.send_to(&req_buffer.buf[0..req_buffer.pos], server));
+
+    let mut res_buffer = BytePacketBuffer::new();
+    socket.recv_from(&mut res_buffer.buf).unwrap();
+
+    DnsPacket::from_buffer(&mut res_buffer)
+}
